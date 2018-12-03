906c328fdbc30fedc2e502503fba9c7d4eccc0f1
wanfu2 <1024536231@qq.com>
2018-12-03 22:15:12 +0800

    1.初步提交

---
+++ webCDN/js/footer.js
@@ -0,0 +1,13 @@
+Vue.component('app-footer', {
+    template: `
+        <div class="box-flex align-center text-center flex-column">
+            <div>2018 © <a href="#" class="p-l-m p-r-m">openauth.me</a>LGPL license 官方技术交流群：484498493 【已满】<span class="p-l-m p-r-m">626433139【已满】</span>566344079 </div>
+            <div>
+                <a class="p-r-sm" href="http://www.openauth.me/questions/detail?id=a2be2d61-7fcb-4df8-8be2-9f296c22a89c">商务合作/授权说明</a>
+                <a class="p-l-sm" href="https://git.oschina.net/yubaolee/OpenAuth.Net" target="_blank">Git仓库</a>
+                <span class="p-l-sm p-r-sm">|</span>
+                <a href="http://www.miibeian.gov.cn" target="_blank">渝ICP备16009992号</a>
+            </div>
+        </div>
+    `
+})
---
+++ webCDN/js/header.js
@@ -0,0 +1,49 @@
+Vue.component('app-header', {
+    props: ['current'],
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1'
+        };
+    },
+    created(){
+        console.log(this.current)
+    },
+    mounted(){
+        console.log(this.current)
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    },
+    template: `
+        <div class="container box-flex">
+            <div class="left-logo box-flex align-center">
+                <div class="nav">
+                    <a href="../../pages/main/index.html"><img src="../../img/logo.png" /></a>
+                </div>
+                <div class="nav p-l-m" :class="current === 'qa'?'active':''">
+                    <a href="../../pages/main/index.html"><i class="fa fa-question-circle-o fa-fw p-l-m p-r-m" aria-hidden="true"></i>问答</a>
+                </div>
+                <div class="nav p-l-m m-l-llg" :class="current === 'ui'?'active':''">
+                    <a href="../../pages/main/index.html"><i class="fa fa-css3 fa-fw p-l-m p-r-m" aria-hidden="true"></i>框架</a>
+                </div>
+            </div>
+            <div class="right-nav flex-1 justify-end box-flex">
+                <div class="nav m-r-llg">
+                    <i class="fa fa-user-circle fa-fw p-r-m" aria-hidden="true"></i>登录
+                </div>
+                <div class="nav m-l-llg">
+                        注册
+                </div>
+                <div class="nav m-l-llg">
+                    <i class="fa fa-qq fa-fw m-l-llg" aria-hidden="true"></i>
+                </div>
+                <div class="nav m-l-llg">	
+                    <i class="fa fa-wechat fa-fw" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+    `
+})
