20d003fdd3c55778460e66251ccbc314f7d05e8d
wanfu2 <1024536231@qq.com>
2018-12-05 22:09:56 +0800

    1.出不提交 user/index.html 与 axios

---
+++ webCDN/js/common.js
@@ -0,0 +1,50 @@
+const  Message = new Vue().$message
+const service = axios.create({
+    baseURL:'http://119.84.146.233:8889', // api 的 base_url
+    timeout: 25000 ,// 请求超时时间
+    headers: {
+        'Content-type': 'application/json;charset=UTF-8'
+    },
+  })
+  
+  // request拦截器
+  service.interceptors.request.use(
+    config => {
+        return config
+    },
+    error => {
+      // Do something with request error
+      console.log(error) // for debug
+      Promise.reject(error)
+    }
+  )
+  
+  // response 拦截器
+  service.interceptors.response.use(
+    response => {
+      /**
+       * code为非20000是抛错 可结合自己业务进行修改
+       */
+      const res = response.data
+      if (res.code !== 200) {
+        Message({
+          message: res.resultMessage,
+          type: 'error',
+          duration: 5 * 1000
+        })
+        return Promise.reject('error')
+      } else {
+        return response.data
+      }
+    },
+    error => {
+      console.log('err' + error) // for debug
+      Message({
+        message: error.message,
+        type: 'error',
+        duration: 5 * 1000
+      })
+      return Promise.reject(error)
+    }
+  )
+  
