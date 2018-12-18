cd2778e992d82318a4af668374a3d97307e43127
wanfu2 <1024536231@qq.com>
2018-12-18 22:19:24 +0800

    1.详情页

--- webCDN/js/common.js
+++ webCDN/js/common.js
@@ -39,3 +39,10 @@ const service = axios.create({
     }
   )
   
+//获取url中的字段
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r != null) return unescape(r[2]);
+	return null;
+}
