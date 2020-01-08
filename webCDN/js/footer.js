6bb0577ffb5d9003fd1a126ef507044d344e1056
zhongxiaojun <djttasso@qq.com>
2020-01-08 17:23:17 +0800

    调整文件

--- webCDN/js/footer.js
+++ webCDN/js/footer.js
@@ -1,12 +1,9 @@
 Vue.component('app-footer', {
     template: `
         <div class="box-flex align-center text-center flex-column">
-            <div>2018 © <a href="#" class="p-l-m p-r-m">openauth.me</a>LGPL license 官方技术交流群：484498493 【已满】<span class="p-l-m p-r-m">626433139【已满】</span>566344079 </div>
+            <div>2020 © <a href="#" class="p-l-m p-r-m">邹邹西语</a>GPL3.0 license 官方交流群：897188768   </div>
             <div>
-                <a class="p-r-sm" href="http://www.openauth.me/questions/detail?id=a2be2d61-7fcb-4df8-8be2-9f296c22a89c">商务合作/授权说明</a>
-                <a class="p-l-sm" href="https://git.oschina.net/yubaolee/OpenAuth.Net" target="_blank">Git仓库</a>
-                <span class="p-l-sm p-r-sm">|</span>
-                <a href="http://www.miibeian.gov.cn" target="_blank">渝ICP备16009992号</a>
+                
             </div>
         </div>
     `
--- webCDN/js/header.js
+++ webCDN/js/header.js
@@ -1,43 +1,41 @@
 Vue.component('app-header', {
-	props: ['current'],
-	data() {
-		return {
-			activeIndex: '1',
-			activeIndex2: '1',
-			user: {
-
-			},
-			isLogin: false
-		}
-	},
-	created() {
-		var isLogin = sessionStorage.isLogin
-		if(isLogin === 'true') {
-			var user = sessionStorage.user
-			user = JSON.parse(user);
-			this.user = user
-			this.isLogin = true
-		}
-	},
-	mounted() {
-		console.log(this.current)
-	},
-	methods: {
-		handleSelect(key, keyPath) {
-			console.log(key, keyPath);
-		},
-		loginOut() {
-			this.$confirm('是否退出登录?', '退出登录', {
-				confirmButtonText: '确定',
-				cancelButtonText: '取消',
-				type: 'warning'
-			}).then(() => {
-				//to do loginOut
-				sessionStorage.isLogin = false
-				sessionStorage.user = ''
-				setTimeout(function() {
-						window.location.href = "../question/index.html"
-					}, 1000)
+    props: ['current'],
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1',
+            user: {},
+            isLogin: false
+        }
+    },
+    created() {
+        var isLogin = sessionStorage.isLogin
+        if (isLogin === 'true') {
+            var user = sessionStorage.user
+            user = JSON.parse(user);
+            this.user = user
+            this.isLogin = true
+        }
+    },
+    mounted() {
+        console.log(this.current)
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        loginOut() {
+            this.$confirm('是否退出登录?', '退出登录', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                //to do loginOut
+                sessionStorage.isLogin = false
+                sessionStorage.user = ''
+                setTimeout(function () {
+                    window.location.href = "../question/index.html"
+                }, 1000)
 //				service({
 //					url: '/user/logout',
 //					method: 'get',
@@ -56,21 +54,24 @@ Vue.component('app-header', {
 //						window.location.href = "../question/index.html"
 //					}, 1000)
 //				})
-			})
-		}
-	},
-	template: `
+            })
+        }
+    },
+    template: `
         <div class="container box-flex">
             <div class="left-logo box-flex align-center">
                 <div class="nav">
                     <a href="../../pages/question/index.html"><img src="../../img/logo.png" /></a>
                 </div>
-                <div class="nav p-l-m" :class="current === 'qa'?'active':''">
-                    <a href="../../pages/question/index.html"><i class="fa fa-question-circle-o fa-fw p-l-m p-r-m" aria-hidden="true"></i>问答</a>
-                </div>
-                <div class="nav p-l-m m-l-llg" :class="current === 'ui'?'active':''">
-                    <a href="../../pages/main/index.html"><i class="fa fa-css3 fa-fw p-l-m p-r-m" aria-hidden="true"></i>框架</a>
-                </div>
+                <!--<div class="nav p-l-m" :class="current === 'qa'?'active':''">-->
+                    <!--<a href="../../pages/question/index.html"><i class="fa fa-question-circle-o fa-fw p-l-m p-r-m" aria-hidden="true"></i>问答</a>-->
+                <!--</div>-->
+                <!---->
+                <!--<div  class="nav p-l-m m-l-llg" :class="current === 'ui'?'active':''"    >-->
+              <!---->
+                    <!--<a href="../../pages/main/index.html"><i class="fa fa-css3 fa-fw p-l-m p-r-m" aria-hidden="true"></i>框架</a>-->
+                <!--</div>-->
+                
             </div>
             <div class="right-nav flex-1 justify-end box-flex">
                 <template v-if="!isLogin">
@@ -81,10 +82,10 @@ Vue.component('app-header', {
 	                 <a href="../../pages/user/login.html?current='register'">注册</a>
 	                </div>
 	                <div class="nav m-l-llg">
-	                    <i class="fa fa-qq fa-fw m-l-llg" aria-hidden="true"></i>
+	                    <i class="fa fa-qq fa-fw m-l-llg" style="display:none"   aria-hidden="true"></i>
 	                </div>
 	                <div class="nav m-l-llg">	
-	                    <i class="fa fa-wechat fa-fw" aria-hidden="true"></i>
+	                    <i class="fa fa-wechat fa-fw"  style="display:none"   aria-hidden="true"></i>
 	                </div>
                 </template>
                  <template v-else>
